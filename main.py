f5f4c568391dedbf6a0e92a4d23ca2583374c4fd
Akber <derasser08@gmail.com>
2026-08-30 11:58:43 +0400

    Added GET request sending and response obtaining

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import validators
 from urllib.parse import urlparse
+from request_handler import send_request
+
 print("Web Security Header Analyzer 0.1")
 program = True
 while program:
     target = input("Enter the target URL: ")
     parsed_target = urlparse(target)
     if validators.url(target):
-        print("URL Successful!")
         program = False
     else:
         print("Invalid URL. Try again!")
-        
+
+response = send_request(target)
+print(f"Status code: {response.status_code}")
+
+    
